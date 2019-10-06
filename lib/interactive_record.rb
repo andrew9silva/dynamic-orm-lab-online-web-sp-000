aece8895dee1c6e710d3a97eeae845bdf2dd0d5c
Andrew Silva <andrewsilva9@gmail.com>
2019-10-06 17:13:10 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -36,6 +36,9 @@ class InteractiveRecord
 
   def values_for_insert
     values = []
+
+    self.class.column_names.each do |column_name|
+      values << "'#{send(column_name)}'" unless send(column_name).nil?
   end
 
 end
